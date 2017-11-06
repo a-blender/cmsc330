f12d27016195aad2dd8de675ef9309707da33417
Anna Michelle Blendermann <ablender@grace6.umd.edu>
2017-11-06 01:46:44 -0500

    debugged the first stages of the lexer

--- p4/lexer.ml
+++ p4/lexer.ml
@@ -3,22 +3,22 @@ open SmallCTypes
 let tokenize input = 
 	
 	let re_while = Str.regexp "while" in
-	let re_type_int = Str.regexp "[0-9]" in
+	let re_int = Str.regexp "-?[0-9]+" in
 
 	(* inner function to process the current token *)
 	let rec next_token str pos = 
 		if pos >= (String.length str) 
-			then [Tok_END]
+			then [EOF]
 		
 		else if (Str.string_match re_while str pos) 
 			then let token = Str.matched_string str in
-			(Tok_WhileNum)::(next_token str (pos+(String.length token)))
+			(Tok_While)::(next_token str (pos+(String.length token)))
 			
-		else if (Str.string_match re_type_int str pos) 
+		else if (Str.string_match re_int str pos) 
 			then let token = Str.matched_string str in
-			(Tok_Type_Int token.[0])::(next_token str (pos+1))
+			(Tok_Int (int_of_string token))::(next_token str (pos+(String.length token)))
 		
-		else raise (IllegalExpression "tokenize")
+		else raise (InvalidInputException "Lex Error")
 	in
 
 (* call next_token on the start position of input *)		
