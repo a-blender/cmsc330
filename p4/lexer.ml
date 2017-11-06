719759841fea0f731a098c87f5c945af7adde4c5
Anna Michelle Blendermann <ablender@grace6.umd.edu>
2017-11-06 04:02:52 -0500

    debugged regex problems and sent files back to local system

--- p4/lexer.ml
+++ p4/lexer.ml
@@ -14,8 +14,8 @@ let tokenize input =
 	let re_greaterequal = Str.regexp ">=" in
 	let re_lessequal = Str.regexp "<=" in
 	
-	let re_or = Str.regexp "[\||]" in 
-	let re_and = Str.regexp "[\&&]" in
+	let re_or = Str.regexp "||" in
+	let re_and = Str.regexp "&&" in
 	let re_not = Str.regexp "!" in
 	let re_semi = Str.regexp ";" in
 	let re_type_int = Str.regexp "int" in (*continue here *)
@@ -30,7 +30,7 @@ let tokenize input =
 	let re_sub = Str.regexp ">" in
 	let re_mult = Str.regexp ">" in
 	let re_div = Str.regexp "/" in
-	let re_pow = Str.regexp "[\^]" in
+	let re_pow = Str.regexp "\\^" in
 
 	(* tokens with complex rules *)
 	let re_bool = Str.regexp "true\\|false" in
@@ -38,7 +38,7 @@ let tokenize input =
 	let re_id = Str.regexp "[a-zA-Z][a-zA-Z0-9]*" in
 	
 	let re_skip = Str.regexp "[ \t\n]*" in
-	let re_extra = Str.regexp "[a-zA-Z0-9]*" in
+	let re_extra = Str.regexp "[a-zA-Z0-9]+" in
 
 	(* inner function to process the current token *)
 	let rec next_token str pos = 
@@ -47,80 +47,65 @@ let tokenize input =
 		
 		(* Tok_LParen *)
 		else if (Str.string_match re_lparen str pos) 
-			then let token = Str.matched_string str in
-			(Tok_LParen)::(next_token str (pos+1))
+			then (Tok_LParen)::(next_token str (pos+1))
 			
 		(* Tok_RParen *)
 		else if (Str.string_match re_rparen str pos) 
-			then let token = Str.matched_string str in
-			(Tok_RParen)::(next_token str (pos+1))
+			then (Tok_RParen)::(next_token str (pos+1))
 		
 		(* Tok_LBrace *)	
 		else if (Str.string_match re_lbrace str pos) 
-			then let token = Str.matched_string str in
-			(Tok_LBrace)::(next_token str (pos+1))
+			then (Tok_LBrace)::(next_token str (pos+1))
 		
 		(* Tok_RBrace *)	
 		else if (Str.string_match re_rbrace str pos) 
-			then let token = Str.matched_string str in
-			(Tok_RBrace)::(next_token str (pos+1))
+			then (Tok_RBrace)::(next_token str (pos+1))
 			
 		(* match the longer operators first *)
 		(* Tok_Equal *)
 		else if (Str.string_match re_equal str pos) 
-			then let token = Str.matched_string str in
-			(Tok_Equal)::(next_token str (pos+2))
+			then (Tok_Equal)::(next_token str (pos+2))
 			
 		(* Tok_NotEqual *)
 		else if (Str.string_match re_notequal str pos) 
-			then let token = Str.matched_string str in
-			(Tok_NotEqual)::(next_token str (pos+2))	
+			then (Tok_NotEqual)::(next_token str (pos+2))	
 			
 		(* Tok_GreaterEqual *)	
 		else if (Str.string_match re_greaterequal str pos) 
-			then let token = Str.matched_string str in
-			(Tok_GreaterEqual)::(next_token str (pos+2))
+			then (Tok_GreaterEqual)::(next_token str (pos+2))
 		
 		(* Tok_LessEqual *)	
 		else if (Str.string_match re_lessequal str pos) 
-			then let token = Str.matched_string str in
-			(Tok_LessEqual)::(next_token str (pos+2))	
+			then (Tok_LessEqual)::(next_token str (pos+2))	
 		
 		(* now match the one char operators *)
 		(* Tok_Assign *)
 		else if (Str.string_match re_assign str pos) 
-			then let token = Str.matched_string str in
-			(Tok_Assign)::(next_token str (pos+1))	
+			then (Tok_Assign)::(next_token str (pos+1))	
 			
 		(* Tok_Greater *)
 		else if (Str.string_match re_greater str pos) 
-			then let token = Str.matched_string str in
-			(Tok_Greater)::(next_token str (pos+1))
+			then (Tok_Greater)::(next_token str (pos+1))
 			
 		(* Tok_Less *)
 		else if (Str.string_match re_less str pos) 
-			then let token = Str.matched_string str in
-			(Tok_Less)::(next_token str (pos+1))
+			then (Tok_Less)::(next_token str (pos+1))
 			
 		(* Tok_Or *)
 		else if (Str.string_match re_or str pos) 
-			then let token = Str.matched_string str in
-			(Tok_Or)::(next_token str (pos+2))
+			then (Tok_Or)::(next_token str (pos+2))
 			
 		(* Tok_And *)
 		else if (Str.string_match re_and str pos) 
-			then let token = Str.matched_string str in
-			(Tok_And)::(next_token str (pos+2))
+			then (Tok_And)::(next_token str (pos+2))
 		
 		(* Tok_Not *)	
 		else if (Str.string_match re_not str pos) 
-			then let token = Str.matched_string str in
-			(Tok_Not)::(next_token str (pos+1))
+			then (Tok_Not)::(next_token str (pos+1))
 			
 		(* Tok_Semi *)	
 		else if (Str.string_match re_semi str pos) 
-			then let token = Str.matched_string str in
-			(Tok_Semi)::(next_token str (pos+1))
+			then (Tok_Semi)::(next_token str (pos+1))
 				
 		(* match for keywords *)
 		(* if there are chars/ints after then match tok_id *)
@@ -128,24 +113,23 @@ let tokenize input =
 		(* Tok_Type_Int *)			
 		else if (Str.string_match re_type_int str pos) 
 			then let token_key = Str.matched_string str in
+			let new_pos = Str.match_end() in
 			
-			if (Str.string_match re_extra str Str.match_end())
+			if (Str.string_match re_extra str new_pos)
 				then let token_id = Str.matched_string str in
-				(Tok_ID)::(next_token str Str.match_end())
-			else (Tok_Type_Int (token_key^token_id)::(next_token str Str.match_end())
+				(Tok_ID (token_key^token_id))::(next_token str (Str.match_end()))
+			else (Tok_Type_Int)::(next_token str new_pos)
 		
 		(* ADD MORE TOKENS HERE *)
 		
 		else if (Str.string_match re_while str pos) 
-			then let token = Str.matched_string str in
-			(Tok_While)::(next_token str Str.match_end())
+			then (Tok_While)::(next_token str (Str.match_end()))
 		
 		(* FINISH ADDING TOKENS HERE *)
 						
-		(* not preferring longest match - FIX THIS *)	
-		else if (Str.string_match re_int str pos) 
-			then let token = Str.matched_string str in
-			(Tok_Int (int_of_string token))::(next_token str Str.match_end())
+		else if (Str.string_match re_int str pos)
+			then let token = Str.matched_string str in 
+			(Tok_Int (int_of_string token))::(next_token str (Str.match_end()))
 		
 		else raise (InvalidInputException "Lex Error")
 	in
