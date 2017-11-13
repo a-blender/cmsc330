44377c85f35dd11190a2bc7e998047e6a9f983c7
Anna Michelle Blendermann <ablender@grace6.umd.edu>
2017-11-13 00:56:37 -0500

    wrote most of the eval_expr function for the interpreter

---
+++ p4b/eval.ml
@@ -0,0 +1,139 @@
+(* EVALUATOR By Anna Blendermann *)
+(* UMD ID: 114474025 *)
+
+open Types
+open Utils
+
+exception TypeError of string
+exception DeclarationError of string
+exception DivByZeroError
+
+(* extend function *)
+let extend env x v = (x,v)::env
+;;
+
+(* search function *)
+let rec search env x = match env with
+	| [] -> false
+	| (y,z)::t -> if x=y then true else (search t x) 
+
+(* lookup function *)
+let rec lookup env x = match env with
+	| [] -> failwith "no environment vars"
+	| (y,z)::t -> if x=y then z else (lookup t x)
+;;
+
+(* eval_expr function *)
+(* evaluates an expression to an int or bool value *)
+let rec eval_expr env e = match e with
+
+
+	| Int x -> Val_Int x
+
+
+	| Bool x -> Val_Bool x
+
+
+	| Id x -> if (search env x)=false
+		then raise (DeclarationError("Id not found"))
+		else (lookup env x)
+
+	| Plus (e1,e2) -> let num1 = (eval_expr env e1) in
+		let num2 = (eval_expr env e2) in
+		(match num1 with 
+		| Val_Int x -> (match num2 with
+			| Val_Int y -> (Val_Int (x+y))
+			| Val_Bool y -> raise (TypeError("plus2 bool found")))
+		| Val_Bool x -> raise (TypeError("plus1 bool found")))
+
+	
+	| Sub (e1,e2) -> let num1 = (eval_expr env e1) in
+		let num2 = (eval_expr env e2) in
+		(match num1 with
+		| Val_Int x -> (match num2 with
+			| Val_Int y -> (Val_Int (x-y))
+			| Val_Bool y -> raise (TypeError("sub1 bool found")))
+		| Val_Bool x -> raise (TypeError("sub2 bool found")))
+
+
+	| Mult (e1,e2) -> let num1 = (eval_expr env e1) in
+		let num2 = (eval_expr env e2) in
+		(match num1 with
+		| Val_Int x -> (match num2 with
+			| Val_Int y -> (Val_Int (x*y))
+			| Val_Bool y -> raise (TypeError("mult2 bool found")))
+		| Val_Bool x -> raise (TypeError("mult1 bool found")))
+
+
+	| Div (e1,e2) -> let num1 = (eval_expr env e1) in
+		let num2 = (eval_expr env e2) in
+		(match num1 with 
+		| Val_Int x -> (match num2 with
+			| Val_Int y -> if y=0 
+				then raise (DivByZeroError) else (Val_Int (x/y))
+			| Val_Bool y -> raise (TypeError("div2 bool found")))
+		| Val_Bool x -> raise (TypeError("div1 bool found")))
+
+
+	| Pow (e1,e2) -> let num1 = (eval_expr env e1) in
+		let num2 = (eval_expr env e2) in
+		(match num1 with
+		| Val_Int x -> (match num2 with
+			| Val_Int y -> 
+				let z = ((float_of_int x)**(float_of_int y)) 
+				in (Val_Int (int_of_float z))
+			| Val_Bool y -> raise (TypeError("pow2 bool found")))
+		| Val_Bool x -> raise (TypeError("pow1 bool found")))
+	
+	
+	| Or (e1,e2) -> let bool1 = (eval_expr env e1) in
+		let bool2 = (eval_expr env e2) in
+		(match bool1 with
+		| Val_Bool x -> (match bool2 with
+			| Val_Bool y -> (Val_Bool (x || y))
+			| Val_Int y -> raise (TypeError("or2 int found")))
+		| Val_Int x -> raise (TypeError("or1 int found")))
+
+
+	| And (e1,e2) -> let bool1 = (eval_expr env e1) in
+		let bool2 = (eval_expr env e2) in
+		(match bool1 with
+		| Val_Bool x -> (match bool2 with
+			| Val_Bool y -> (Val_Bool (x && y))
+			| Val_Int y -> raise (TypeError("and2 int found")))
+		| Val_Int x -> raise (TypeError("and1 int found")))
+
+	
+	| Not exp -> let bool = (eval_expr env exp) in
+		(match bool with
+		| Val_Bool x -> (Val_Bool (!x))
+		| Val_Int x -> raise (TypeError("not (func) int found")))
+
+	
+	| Greater
+
+
+	| Less
+
+
+	| GreaterEqual
+
+
+	| LessEqual
+
+
+	| Equal
+
+
+	| Not Equal
+
+
+	| _ -> raise (TypeError("no valid expression found")) 
+;;
+
+(* eval_stmt function *)
+let rec eval_stmt env s = failwith "unimplemented"
+
+
+
+
